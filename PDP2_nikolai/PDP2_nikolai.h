b35ff56eefeaa5a879d4c02208d944b3e19d80cb
Nikolai <kolomeitsev_n@auca.kg>
2019-11-26 19:49:13 +0600

    Add files via upload

---
+++ PDP2_nikolai/PDP2_nikolai.h
@@ -0,0 +1,14 @@
+#ifndef hInterfaceLib
+#define hInterfaceLib
+
+#include <stdio.h>
+
+#include "gpu_main.h"
+#include "animate.h"
+#include "point.h"
+
+void animateAttractor();
+
+int runIt(GPU_Palette* P1,  CPUAnimBitmap* A1);
+
+#endif
