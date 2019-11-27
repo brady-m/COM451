d0f77f7016f113ac251115e1b920989d406f3081
Dimas1234567890 <43944479+Dimas1234567890@users.noreply.github.com>
2019-11-27 18:00:43 +0600

    Update VisAssign2.cpp

--- VisAssign2.cpp
+++ VisAssign2.cpp
@@ -80,19 +80,23 @@ int firstInit(APoint& thePoint) {
 
 int coor(APoint& thePoint, APoint& theChange) {
 
-	const double t = 0.005;
-		theChange.x = t * (theChange.x * (thePoint.y - thePoint.x));
-		theChange.y = t * ((thePoint.x * (theChange.x - thePoint.z)) - thePoint.y);
-	  theChange.z = t * ( (thePoint.x * thePoint.y) - (theChange.z * thePoint.z));
+	 const double t = 0.005;
+		static float a = 40.0;
+		static float b = 3.0;
+		static float c = 28.0;
+
+		theChange.x = t * (a * (thePoint.y - thePoint.x));
+		theChange.y  = t * ((thePoint.x * (c-a)) - (thePoint.x * thePoint.z) + (c * thePoint.y));
+		theChange.z  = t * ((thePoint.x * thePoint.y) - (b * thePoint.z));
 
 	  thePoint.x += theChange.x;
 	  thePoint.y += theChange.y;
 	  thePoint.z += theChange.z;
 
-	  static float minX = -20.0;
-	  static float maxX = 20.0;
-	  static float minY = -30.0;
-	  static float maxY = 30.0;
+	  static float minX = -50.0;
+	  static float maxX = 50.0;
+	  static float minY = -150.0;
+	  static float maxY = 150.0;
 
 	  static float xRange = fabs(maxX - minX);
 	  static float xScalar = 0.9 * (gWIDTH/xRange);
@@ -102,34 +106,8 @@ int coor(APoint& thePoint, APoint& theChange) {
 
 	  thePoint.xIdx = round(xScalar * (thePoint.x - minX));
 	  thePoint.yIdx = round(yScalar * (thePoint.y - minY));
-	// float sigma = 10.0;
-	// float rho = 28.0;
-	// float beta = 2.666;
-	//
-	// theChange.x = t * (sigma * (thePoint.y - thePoint.x));
-	// theChange.y = t * ( (thePoint.x * (rho - thePoint.z)) - thePoint.y);
-	// theChange.z = t * ( (thePoint.x * thePoint.y) - (beta * thePoint.z) );
-	//
-	//
-  //   thePoint.x += theChange.x;
-  //   thePoint.y += theChange.y;
-  //   thePoint.z += theChange.z;
-	//
-  //   static float minX = -20;
-  //   static float maxX = 20;
-  //   static float minY = -30;
-  //   static float maxY = 30;
+	
 
-    // static float xRange = fabs(maxX - minX);
-    // static float xScalar = 0.9 * (gWIDTH/xRange);
-		//
-    // static float yRange = fabs(maxY - minY);
-    // static float yScalar =  0.9 * (gHEIGHT/yRange);
-		//
-
-		//
-    // thePoint.xIdx = floor((thePoint.y * 32)) * 5.0; // (X * scalar) + (gWidth/2)
-    // thePoint.yIdx = floor((thePoint.x * 18)) * 5.0; // (Y * scalar) + (gHeight/2)
 
     return 0;
 }
@@ -161,36 +139,6 @@ int runIt(GPU_Palette* P1, CPUAnimBitmap* A1){
 
 	for (long i = 1; i< 100000; i++)
 	{
-
-		// theChange.x = t * (sigma * (thePoint.y - thePoint.x));
-		// theChange.y = t * ( (thePoint.x * (rho - thePoint.z)) - thePoint.y);
-		// theChange.z = t * ( (thePoint.x * thePoint.y) - (beta * thePoint.z) );
-
-		// thePoint.x += theChange.x;
-		// thePoint.y += theChange.y;
-		// thePoint.z += theChange.z;
-
-		// xIdx = floor((thePoint.x * 32) + 960); // (X * scalar) + (gWidth/2)
-		// yIdx = floor((thePoint.y * 18) + 540); // (Y * scalar) + (gHeight/2)
-		//
-		// xIdx1 = floor((thePoint.x * 30) + 90); // (X * scalar) + (gWidth/2)
-		// yIdx1 = floor((thePoint.y * 16) + 54); // (Y * scalar) + (gHeight/2)
-		//
-		// xIdx2 = floor((thePoint.x * 28) + 820); // (X * scalar) + (gWidth/2)
-		// yIdx2 = floor((thePoint.y * 14) + 320); // (Y * scalar) + (gHeight/2)
-		//
-		// xIdx3 = floor((thePoint.x * 26) + 210); // (X * scalar) + (gWidth/2)
-		// yIdx3 = floor((thePoint.y * 12) + 450); // (Y * scalar) + (gHeight/2)
-		//
-		// xIdx4 = floor((thePoint.x * 24) + 330); // (X * scalar) + (gWidth/2)
-		// yIdx4 = floor((thePoint.y * 10) + 540); // (Y * scalar) + (gHeight/2)
-		//
-		// updatePalette(P1, xIdx, yIdx, thePoint.z);
-		// updatePalette(P1, xIdx1, yIdx1, thePoint.z);
-		// updatePalette(P1, xIdx2, yIdx2, thePoint.z);
-		// updatePalette(P1, xIdx3, yIdx3, thePoint.z);
-		// updatePalette(P1, xIdx4, yIdx4, thePoint.z);
-    // A1->drawPalette();
 
 		for (int i = 0; i < ATTRACTORS; i++) {
 				threads[i] = std::thread(coor, std::ref(points[i]), std::ref(changes[i]));
